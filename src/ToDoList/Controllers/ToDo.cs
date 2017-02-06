260d9a0c5425e702874e3b047a54cb77397b7bca
Dustin Nebgen <speedcomputersdn@gmail.com>
2017-02-05 21:38:46 -0600

    Make ToDo into own Controller and start work on layout

--- src/ToDoList/Controllers/ToDo.cs
+++ src/ToDoList/Controllers/ToDo.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace ToDoList.Controllers
 {
-    public class HomeController : Controller
+    public class ToDo : Controller
     {
+        // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult Error()
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Edit()
         {
             return View();
         }
